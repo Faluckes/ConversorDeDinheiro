707e6f1b4bc510eab8f9c8f37f915fdb59dea27b
Faluckes <FaluckesDev@protonmail.com>
2022-03-15 01:17:49 -0300

    Adicionar arquivos de projeto.

---
+++ ConversorDeDinheiro/ExtratorValorDeArgumentosURL.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeDinheiro
+{
+    public class ExtratorValorDeArgumentosURL
+    {
+
+        private readonly string _argument;
+        public string URL { get;}
+
+
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+            if (String.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("O argumento não pode ser vazio ou nulo", nameof(url));
+            }
+
+               URL = url;
+
+            int indiceInterrogacao = URL.IndexOf('?');
+
+            _argument = URL.Substring(indiceInterrogacao + 1);
+
+        }
+
+        // moedaOrigem=real&moedaDestino=dolar
+        public string GetValor(string nomeParametro)
+        {
+            nomeParametro = nomeParametro.ToUpper();
+            string argumentCaixaAlta = _argument.ToUpper(); // MOEDAORIGEM=REAL&MOEDADESTINO=DOLAR&VALOR
+
+            string termo = nomeParametro + "="; //moedaDestino
+            int indiceTermo = argumentCaixaAlta.IndexOf(termo); // x
+
+            string resultado = _argument.Substring(indiceTermo + termo.Length); // dolar
+            int indiceEComercial = resultado.IndexOf("&");  
+            
+            if (indiceEComercial == -1)
+            {
+                return resultado;
+            }
+
+            return resultado.Remove(indiceEComercial);  
+        }
+
+
+    }
+}
---
+++ ConversorDeDinheiro/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConversorDeDinheiro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //pagina?argumento
+            //012345678
+
+
+            string urlParametros = "https://www.ConverterMoney.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=4000";
+            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
+
+            string moedaDestino = extratorDeValores.GetValor("moedaDestino");
+            Console.WriteLine($"Valor moedaDestino: {moedaDestino}");
+            string moedaOrigem = extratorDeValores.GetValor("moedaOrigem");
+            Console.WriteLine($"Valor moedaOrigem {moedaOrigem}");
+            Console.WriteLine(extratorDeValores.GetValor("Valor"));
+
+
+
+            Console.ReadLine();
+
+            //ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(null);
+
+            //string url = "pagina?modedaOrigem=real&moedaDestino=dolar";
+
+            //int indiceInterrogacao = url.IndexOf('?');
+
+            //Console.WriteLine(indiceInterrogacao);
+
+            //Console.WriteLine(url);
+
+            //string argumentos = url.Substring(indiceInterrogacao + 1);
+
+            //Console.WriteLine(argumentos);
+
+            //Console.ReadLine();
+        }
+    }
+}
