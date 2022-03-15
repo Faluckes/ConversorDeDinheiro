98b59b8a13ee5e25687d4b28501d25c0140ae606
Faluckes <FaluckesDev@protonmail.com>
2022-03-15 20:43:45 -0300

    Course completed.

--- ConversorDeDinheiro/Program.cs
+++ ConversorDeDinheiro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace ConversorDeDinheiro
 {
@@ -6,6 +7,33 @@ namespace ConversorDeDinheiro
     {
         static void Main(string[] args)
         {
+
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+            string textoTeste = "Meu nome é Faluckes, me ligue em 987515619";
+
+            Match resultado = Regex.Match(textoTeste, padrao);
+            Console.WriteLine(resultado.Value);
+
+            Console.ReadLine();
+
+
+
+
+
+
+            string urlTestee = "https://www.ConverterMoney.com/cambio";
+            int indiceConverterMoney = urlTestee.IndexOf("https://www.ConverterMoney.com");
+            Console.WriteLine(urlTestee.StartsWith("https://www.ConverterMoney.com"));
+            Console.WriteLine(urlTestee.EndsWith("cambio"));
+
+            Console.WriteLine(urlTestee.Contains("converterMoney"));
+
+
+            Console.WriteLine(indiceConverterMoney == 0);
+            Console.ReadLine();
+
+
+
             //pagina?argumento
             //012345678
 
